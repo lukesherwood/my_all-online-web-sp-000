edc68ce8329539cc8d722efa2ece85b35cfffa6d
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-31 22:07:03 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,8 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-i = 0
-while i < collection.length
-  
-i +=1
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values <<
+yield(collection[i])
+    i +=1
+  end
 end
